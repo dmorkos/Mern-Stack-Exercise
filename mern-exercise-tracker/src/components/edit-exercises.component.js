4f439d750740b3dad3f53396d943fb7330386abe
dmorkos1@hotmail.com <dmorkos1@hotmail.com>
2019-09-15 23:18:11 +1000

    FInished exercise-Tracker

--- mern-exercise-tracker/src/components/edit-exercises.component.js
+++ mern-exercise-tracker/src/components/edit-exercises.component.js
@@ -1,11 +1,154 @@
 import React, {Component} from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import axios from 'axios';
 
-export default class  EditExercises extends Component {
+export default class EditExercise extends Component {
+    constructor(props) {
+        // In JS class you always need to call super when defining 
+        // the constructor of a subclass 
+        super(props);
+        // setting the initial state of the component 
+        // STate is how you create a variable in react 
+        // this is how we relate this to the methods below
+        this.onChangeUserName = this.onChangeUserName.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            // we will create properties of the state that will correspond to
+            // the mongoDb document
+            username: '',
+            description: '',
+            duration : 0,
+            date: new Date(),
+            users: []
+        }
+    }
+    // this is a react lifecycle method 
+    // will automatically get called right before anything gets displayed on the screen
+    componentDidMount() {
+        axios.get('http://localhost:5000/exercises/'+this.props.match.params.id)
+        .then(response => {
+            this.setState({
+                username: response.data.username,
+                description: response.data.description,
+                duration: response.data.duration,
+                date: new Date (response.data.date)
+            })
+        })
+       axios.get('http://localhost:5000/users/')
+        .then(response => {
+            // now we want to check if there is a response 
+            if(response.data.length > 0) {
+                this.setState({
+                    users: response.data.map(user => user.username), // data will be an array and we will map the array 
+                })
+            }
+        })
+
+    }
+    onChangeUserName(e) {
+       this.setState({
+            username: e.target.value // target is the textbox
+       })
+    }
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value // target is the textbox
+        })
+     }
+     onChangeDuration(e) {
+        this.setState({
+             duration: e.target.value // target is the textbox
+        })
+     }
+     
+     onChangeDate(date) {
+        this.setState({
+             date: date 
+        });
+     }
+     onSubmit(e) {
+         e.preventDefault();
+
+         const exercise = {
+             username: this.state.username,
+             description: this.state.description,
+             duration: this.state.duration,
+             date: this.state.date
+         }
+         console.log(exercise)
+         // adding the exercise in the database by sending a HTTP request 
+         axios.post('http://localhost:5000/exercises/update/'+this.props.match.params.id, exercise)
+            .then(res => console.log(res.data));
+
+         window.location = '/' // got back to home page
+     }
+
+
     render() {
         return (
-            <div>
-                <p>Your in the edit exercise  component</p>
-            </div>
+          <div>
+              <h3>Edit exercise log </h3>
+              <form onSubmit={this.onSubmit}>
+                  <div className="form-group">
+                      <label>UserName</label>
+                      <select ref = "userInput"
+                        required
+                        className="form-control"
+                        value = {this.state.username}
+                        onChange={this.onChangeUserName}>
+                         {
+                             // getting data from the db and using that data to be added to the dropdown method
+                            // This.state.users => give you an array of all the users 
+                            //from mongodb.map allows us to map each element of the array 
+                            // 
+                            this.state.users.map(function(user) {
+                                //it will return an option which is part of the select box
+                                return <option
+                                    key={user}
+                                    value={user}>{user}
+                                    </option>;
+                            })
+                        }
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label>Description</label>
+                        <input type = "test"
+                            required
+                            className="form-control"
+                            value={this.state.description}
+                            onChange = {this.onChangeDescription}
+                            />
+                    </div>
+                    <div className="form-group">
+                        <label>Duration in (m)</label>
+                        <input 
+                            type = "text"
+                            className="form-control"
+                            value={this.state.duration}
+                            onChange = {this.onChangeDuration}
+                            />
+                    </div>
+                    <div className="form-group">
+                        <label>Date: </label>
+                        <div>
+                            <DatePicker
+                                selected={this.state.date}
+                                onChange={this.onChangeDate}
+                            />
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value = "Edit Exercise Log" className="btn btn-primary"/>
+                    </div>
+
+              </form>
+          </div>
         )
     }
 }
--- mern-exercise-tracker/src/components/exercises-list.component.js
+++ mern-exercise-tracker/src/components/exercises-list.component.js
@@ -1,11 +1,86 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 
+// exercise component is a function react component 
+// the key difference between a functional component and a class component is 
+// the lack of state and lifecycle method 
+const Exercise = props => (
+    <tr>
+        <td>{props.exercise.username}</td>
+        <td>{props.exercise.description}</td>   
+        <td>{props.exercise.duration}</td>
+        <td>{props.exercise.date.substring(0,10)}</td>
+        <td>
+            <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href ="#" onClick={() => {props.deleteExercise(props.exercise._id)}}>Delete</a>
+        </td>      
+    </tr>
+)
+// this is a class component 
 export default class ExercisesList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.deleteExercise = this.deleteExercise.bind(this);
+        this.state = {exercises: []}; // initialzing the exercises array
+        
+
+    }
+    // this code will run before the page is rendered 
+    // and add the exercises to the list 
+    componentDidMount() {
+        axios.get('http://localhost:5000/exercises/')
+            .then(response => {
+                this.setState({
+                    exercises: response.data
+                })
+               
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+    deleteExercise(id) {
+        axios.delete('http://localhost:5000/exercises/'+id)
+            .then(res => console.log(res.data));
+            // after we delete the data fom the backend we also have to 
+            //delete the exercise from whats being deleted to the user 
+            // setState react will automatically update the page with that new state 
+
+        this.setState({
+            // for every element (el) in the exercise array we will filter it 
+            // if element does not equal the selected array
+            // _id comes from the mongodb object e
+            exercises: this.state.exercises.filter(el => el._id !== id)
+        })
+
+    }
+    exerciseList() {
+        return this.state.exercises.map(currentexercise => {
+            return <Exercise exercise={currentexercise} deleteExercise = {this.deleteExercise} key = {currentexercise._id}/>;
+        })
+    }
+
     render() {
         return (
-            <div>
-                <p>Your in the exercise list component</p>
-            </div>
+           <div>
+               <h3>Logged exercises</h3>
+               <table className = "table">
+                   <thead className = "thead-light"> 
+                       <tr>
+                           <th>Username</th>
+                           <th>Description</th>
+                           <th>Duration</th>
+                           <th>Date</th>
+                           <th>Actions</th>
+                       </tr>
+                    </thead>
+                    <tbody>
+                        {this.exerciseList()}
+                    </tbody>
+
+               </table>
+           </div>
         )
     }
 }
